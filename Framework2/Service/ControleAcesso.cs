5466695c51add70ad9792c48c3eec4518dbca5b8
LucasBeda <lucas.beda@hotmail.com>
2024-04-13 12:29:50 -0300

    Comit Geral

---
+++ Framework2/Service/ControleAcesso.cs
@@ -0,0 +1,16 @@
+﻿namespace Framework2.Service
+{
+    public static class ControleAcesso
+    {
+        private static bool loginRealizado { get; set; }
+        public static void Login(string usuario, string senha)
+        {
+            loginRealizado = (usuario == "Lkz" && senha == "123");
+        }
+
+        public static bool RetornaLoginRealizado()
+        {
+            return loginRealizado;
+        }
+    }
+}
---
+++ NET5/Program.cs
@@ -0,0 +1,19 @@
+﻿using Framework2.Service;
+using System;
+
+Console.WriteLine("Hello World!");
+
+var a = new
+{
+    Nome = "LkzA",
+    Senha = "123"
+};
+
+Console.WriteLine($"Usuario {a}");
+
+ControleAcesso.Login(a.Nome, a.Senha);
+bool loginFeito = ControleAcesso.RetornaLoginRealizado();
+Console.WriteLine($"Login Realizado? {loginFeito}");
+
+Console.WriteLine("Fim execução código");
+Console.ReadKey();
